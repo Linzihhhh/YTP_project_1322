05787bdccbca946939fd88b42c1640db33035c64
soondro2266 <100667910+soondro2266@users.noreply.github.com>
2022-09-16 10:00:06 +0800

    Basic of Selenium(Edge)

--- Web_Scrapying/selenium_test.py
+++ Web_Scrapying/selenium_test.py
@@ -1,3 +1,13 @@
 from selenium import webdriver
+import os
 
-driver = webdriver.Chrome()
+#open the msedgedriver.exe before runing the code
+edge_options = webdriver.EdgeOptions()
+edge_options.add_argument("--window-size=1920x1080")
+edge_options.add_argument("--headless")
+edge_driver = os.getcwd() + "\\msedgedriver.exe"
+print(edge_driver)
+driver = webdriver.Chrome(options=edge_options, executable_path=edge_driver)
+
+url = 'https://www.youtube.com/'
+driver.get(url)
