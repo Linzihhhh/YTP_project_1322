9c129af3ddf6f3eb2eb400d8b491abf16f89de38
soondro2266 <100667910+soondro2266@users.noreply.github.com>
2022-09-18 22:10:49 +0800

    pixiv

--- Web_scrapying/pixiv_selenium.py
+++ Web_scrapying/pixiv_selenium.py
@@ -1,12 +1,24 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 import time
+import random
 import requests
+
+#the class names on pixiv
+classname = {
+    "account":"sc-bn9ph6-6.degQSE",
+    "password":"sc-bn9ph6-6.hfoSmp",
+    "login":"sc-bdnxRM.jvCTkj.sc-dlnjwi.pKCsX.sc-2o1uwj-7.fguACh.sc-2o1uwj-7.fguACh",
+    "recommend_pic":"sc-d98f2c-0.sc-rp5asc-16.iUsZyY.gtm-toppage-thumbnail-illustration-recommend-works.sc-iJCRrE.hizmCn",
+    "picture_div":"sc-1qpw8k9-1.jOmqKq",
+    "one_picture":"sc-1qpw8k9-3.eFhoug.gtm-expand-full-size-illust",
+    "pictures":"sc-1qpw8k9-3.eAvtpu"
+}
 
 account = '' #your account
 password = '' #your password
@@ -20,35 +32,36 @@ url = 'https://www.pixiv.net/login.php?ref=wwwtop_accounts_index'
 driver.get(url) 
 
 #login
-element = driver.find_element(By.CLASS_NAME, "sc-bn9ph6-6.degQSE")
+element = driver.find_element(By.CLASS_NAME, classname["account"])
 element.send_keys(account)
-element = driver.find_element(By.CLASS_NAME, "sc-bn9ph6-6.hfoSmp")
+element = driver.find_element(By.CLASS_NAME, classname["password"])
 element.send_keys(password)
-button = driver.find_element(By.CLASS_NAME, "sc-bdnxRM.jvCTkj.sc-dlnjwi.pKCsX.sc-2o1uwj-7.fguACh.sc-2o1uwj-7.fguACh")
+button = driver.find_element(By.CLASS_NAME, classname["login"])
 button.click()
 
 #wait until page loaded
-WebDriverWait(driver, 30).until(expected_conditions.presence_of_all_elements_located((By.CLASS_NAME,"sc-9y4be5-1.jtUPOE")))
+WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.CLASS_NAME,classname["recommend_pic"])))
 
 #find the picture
-items = driver.find_elements(By.CLASS_NAME, "sc-d98f2c-0.sc-rp5asc-16.iUsZyY.gtm-toppage-thumbnail-illustration-recommend-works.sc-iJCRrE.hizmCn")
+items = driver.find_elements(By.CLASS_NAME, classname["recommend_pic"])
 tar = items[0]
 for i in items:
     if i.get_attribute("data-gtm-recommend-score") > tar.get_attribute("data-gtm-recommend-score"):
-        tar = i
+        if random.randint(1,100) > 30:
+            tar = i
 goodpicture_url = tar.get_attribute("href")
 
 #go to target picture
 driver.get(goodpicture_url)
-WebDriverWait(driver, 30).until(expected_conditions.presence_of_all_elements_located((By.CLASS_NAME, "sc-1qpw8k9-1.jOmqKq")))
+WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.CLASS_NAME, classname["picture_div"])))
 
 #find the picture url
 url = ''
 try:
-    img = driver.find_element(By.CLASS_NAME, "sc-1qpw8k9-3.eFhoug.gtm-expand-full-size-illust")
+    img = driver.find_element(By.CLASS_NAME, classname["one_picture"])
     url = img.get_attribute("href")
 except:
-    img = driver.find_element(By.CLASS_NAME, "sc-1qpw8k9-3.eAvtpu")
+    img = driver.find_element(By.CLASS_NAME, classname["pictures"])
     url = img.get_attribute("href")
 
 #setup the header and request the picture
