9423bf4a1af01b105748a2ef7ba7ad699ea62fe7
GrandTiger1729 <80200287+GrandTiger1729@users.noreply.github.com>
2022-10-07 11:33:26 +0800

    Create ytdl.py

---
+++ ytdl.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+import yt_dlp, urllib.error
+
+class YoutubeMusicDownloader:
+    def __init__(self):
+        self.ydl_opts = {
+            'format': 'bestaudio', # test
+            'outtmpl': '%(extractor_key)s\%(title)s.%(ext)s',
+            # 'outtmpl': '%(extractor_key)s\%(title)s.mp3',
+            'default_search': 'auto',
+            # 'cookiefile': 'youtube.com_cookies.txt', # <- this can download age-restricted video
+        }
+
+    def download(self, url: str):
+        with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+            try:
+                ydl.download([url])
+            except yt_dlp.utils.DownloadError as DownloadError:
+                if type(DownloadError.exc_info[1]) == urllib.error.URLError:
+                    print('Unknown Url')
+                elif type(DownloadError.exc_info[1]) == yt_dlp.utils.ExtractorError:
+                    print('Join this channel to get access to members-only content like this video, and other exclusive perks.')
+            except Exception as ex:
+                print('\n', type(ex).__name__)
+
+    
+ytdl = YoutubeMusicDownloader()
+ytdl.download("url")
