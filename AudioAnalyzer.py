b17239aef59d27b161e6e7710ca7276734dd6e07
Linzihhhh <88263538+Linzihhhh@users.noreply.github.com>
2022-10-13 14:52:39 +0800

    Update AudioAnalyzer.py

--- AudioAnalyzer.py
+++ AudioAnalyzer.py
@@ -13,19 +13,19 @@ class AudioAnalyzer:
         y,sr=librosa.load(Audio)
         STFT=librosa.stft(y)
         STFT=self.Resize(STFT,time_size)
-        return STFT
+        return STFT#numpy
 
     def Analyze_MFCC(self,Audio,time_size):
         y,sr=librosa.load(Audio)
         MFCC=librosa.feature.mfcc(y=y,sr=sr)
         MFCC=self.Resize(MFCC,time_size)
-        return MFCC
+        return MFCC#numpy
 
     def Analyze_CQT(self,Audio,time_size):
         y,sr=librosa.load(Audio)
         CQT=librosa.feature.chroma_cqt(y=y,sr=sr)
         CQT=self.Resize(CQT,time_size)
-        return CQT
+        return CQT#numpy
 
     def Analyze_ALL(self,Audio,time_size):
         y,sr=librosa.load(Audio)
@@ -37,13 +37,16 @@ class AudioAnalyzer:
     
         CQT=librosa.feature.chroma_cqt(y=y,sr=sr)
         CQT=self.Resize(CQT,time_size)
-        totaldata=[]
-        totaldata.append(STFT)
-        totaldata.append(MFCC)
-        totaldata.append(CQT)
         
-        return totaldata
+        totaldata=np.vstack([STFT,MFCC])
+        totaldata=np.vstack([totaldata,CQT])
+        return totaldata#numpy
     
     def Save(self,data,name):
         np.save(name,data)
 
+'''debug
+a=AudioAnalyzer()
+path='1/1.mp3'
+print(a.Analyze_ALL(path, 5000).shape)
+'''
