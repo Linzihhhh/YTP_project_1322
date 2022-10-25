48706cd80548f2628bb8524565d73b9ccbff0a54
GrandTiger1729 <80200287+GrandTiger1729@users.noreply.github.com>
2022-10-25 10:18:05 +0800

    Update ytdl.py

--- main/ytdl.py
+++ main/ytdl.py
@@ -5,27 +5,14 @@ class YoutubeMusicDownloader:
     def __init__(self):
         pass
     
-    def generate_title(self, url: str):
-        process = subprocess.run(f"youtube-dl --get-id \"{url}\"", capture_output=True)
-        process.check_returncode()
-        id = process.stdout.decode().strip()
-        print(id)
-        process = subprocess.run(f"youtube-dl --get-title \"{url}\"", capture_output=True)
-        process.check_returncode()
-        title = process.stdout.decode().strip()
-        return title + " - " + id
-
-    def download(self, url: str, *, path=None, title=None):
+    def download(self, url: str, *, path=None):
         try:
             path = path or os.getcwd()
-            title = title or self.generate_title(url)
 
-            process = subprocess.run(f"youtube-dl --extract-audio --audio-format wav -o \"{path}\\{title}.wav\" \"{url}\"")
+            process = subprocess.run(f"youtube-dl -i --extract-audio --audio-format wav -o \"{path}/%(title)s-%(id)s.wav\" \"{url}\"")
             process.check_returncode()
-
         except Exception as e:
             print(e)
-            
     
-ytdl = YoutubeMusicDownloader()
-ytdl.download("https://www.youtube.com/watch?v=5MWdriB3EBM")
+# ytdl = YoutubeMusicDownloader()
+# ytdl.download("[URL]")
