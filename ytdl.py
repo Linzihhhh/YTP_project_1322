58c22dcfa78eceabe11af4cd72c85e50b2f0203b
GrandTiger1729 <80200287+GrandTiger1729@users.noreply.github.com>
2022-12-02 10:21:14 +0800

    Update ytdl.py

--- ytdl.py
+++ ytdl.py
@@ -5,9 +5,25 @@ import json
 class YoutubeMusicDownloader:
     def __init__(self):
         pass
-    
-    def download(self, url: str, *, path: str = None):
+
+    def get_ids(self, url: str) -> list:
         try:
+            process = subprocess.run(
+                f'yt-dlp --ignore-errors --get-id "{url}"', capture_output = True)
+            
+            ids = process.stdout.decode().split('\n')
+            ids.pop() # remove the last empty string
+            return ids
+        except Exception as e:
+            print(e)
+    
+    def download_song(self, id: str, *, path: str = None): 
+        """
+        Download a video
+        """
+
+        try:
+            url = f"https://www.youtube.com/watch?v={id}"
             path = path or os.getcwd()
 
             process = subprocess.run(
@@ -15,35 +31,51 @@ class YoutubeMusicDownloader:
         except Exception as e:
             print(e)
 
-    def fetch_comments(self, url: str, *, path: str = None):
+    def fetch_comment(self, id: str, *, path: str = None):
+        """
+        Download comments from a video
+        """
+
         try:
-            path = path or os.getcwd()
+            url = f"https://www.youtube.com/watch?v={id}"
 
+            filename = path + "/" + id
             process = subprocess.run(
-                f'yt-dlp --ignore-errors --get-id "{url}"', capture_output = True)
+                f'yt-dlp --ignore-errors --write-comments --no-download -o "{filename}" {url}')
+
+            with open(f"{filename}.info.json", "r") as f:
+                data = json.load(f)
+
+            data = data["comments"]
             
-            ids = process.stdout.decode().split('\n')
-            ids.pop() # remove the last empty string
+            with open(f"{filename}.json", "w") as f:
+                json.dump(data, f)
+            
+            os.remove(f"{filename}.info.json")
 
+        except Exception as e:
+            print(e)
+
+    def download(self, url: str, *, root: str = None):
+        """
+        Download video/playlist from a given url
+        """
+        try:
+            root = root or os.getcwd()
+
+            ids = self.get_ids(url)
             for id in ids:
-                filename = path + "/" + id
-                process = subprocess.run(
-                    f'yt-dlp --ignore-errors --write-comments --no-download -o "{filename}" "https://www.youtube.com/watch?v={id}"')
-
-                with open(f"{id}.info.json", "r") as f:
-                    data = json.load(f)
-
-                data = data["comments"]
-                
-                with open(f"{id}.json", "w") as f:
-                    json.dump(data, f)
-                
-                os.remove(f"{filename}.info.json")
+                try:
+                    path = root + "/" + id
+                    os.mkdir(path)
+                    
+                    self.download_song(id, path = path)
+                    self.fetch_comment(id, path = path)
+                except Exception as e:
+                    print(e)
 
         except Exception as e:
             print(e)
     
 # ytdl = YoutubeMusicDownloader()
 # ytdl.download("$video/playlist")
-# ytdl.fetch_comments("$playlist")
-# ytdl.fetch_comments("$video")
